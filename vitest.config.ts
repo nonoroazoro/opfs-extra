11d1a2f2aed96a69542119b9a0ebb9710a817bc7
Nonoroazoro <xiaochao.k@gmail.com>
2025-11-29 10:46:36 +0800

    feat: use vitest for testing

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            instances: [{ browser: "chromium" }]
+        }
+    }
+});
